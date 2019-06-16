736074fc70d8efb3445a5b05e22d3b9f7540cb34
unknown <racin_yx@163.com@racin_yx@163.com>
2019-06-16 08:23:40 +0800

    python

---
+++ os_con.py
@@ -0,0 +1,8 @@
+#简单词云分析
+import os
+import os.path as op
+print(op.abspath("os_con.pyin"))   #返回os_con.py的绝对路径
+print(op.isfile("os_con.py"))  #返回os_con.py是否为文件
+print(op.isdir("os_con.py")) #返回os_con.py 是否为目录
+os.system("C:/windows/System32/calc.exe")  #打开计算器
+print(os.cpu_count()) #返回计算机的cpu个数
